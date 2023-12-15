ff54255b51e0c3925d38bae5bbad247813e7ca68
Jayakanth <jayakanth1812@gmail.com>
2023-12-15 20:29:29 +0530

    jwt verify

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
+
 const app = express();
 
-app.get("/", (req, res) => {
-  const token = jwt.sign({ myName: "jk" }, "uniquekey007");
+//generating token
+app.get("/", async (req, res) => {
+  const token = await jwt.sign({ date: new Date() }, "uniquekey007");
   res.json({ message: "homepage", token });
+});
+
+//verifying token
+app.get("/check/:token", async (req, res) => {
+  const { token } = req.params;
+  try {
+    const tokenResult = await jwt.verify(token, "uniquekey007");
+    console.log(tokenResult);
+    res.json({ message: "success" });
+  } catch (error) {
+    res.status(401).json({ error });
+  }
 });
 
 app.listen(8080, () => console.log(`sever is running`));
